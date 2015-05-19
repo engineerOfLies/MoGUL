b417acafa660e29cb29234f8f1a7bb02cab399ba
engineerOfLies <donald.j.kehoe@gmail.com>
2015-05-19 19:27:52 -0400

    adding an all make script until I get cmake configured

---
+++ makeAll.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "building base MoGUL libs..."
+cd mgl_types/src
+make
+make static
+
+cd ../../mgl_logger/src
+make
+make static
+
+cd ../../mgl_resource/src
+make
+make static
+
+cd ../../mgl_config/src
+make
+make static
+
+echo "building core MoGUL libs..."
+
+cd ../../mgl_graphics/src
+make
+make static
+
+echo "done"
