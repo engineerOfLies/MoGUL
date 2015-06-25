672eebb9be180c8a6ff851d6fb1ce9a576d6f99c
engineerOfLies <donald.j.kehoe@gmail.com>
2015-06-25 07:38:15 -0400

    fixed some errors in the license blocks

--- mgl_graphics/include/mgl_actor.h
+++ mgl_graphics/include/mgl_actor.h
@@ -19,8 +19,6 @@
  *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *    SOFTWARE.
- *    @purpose the MGL text library provides common string support for fixed length
- *    strings.  For use with unlimited strings mgl will use glib's GString type
  */
 
 /**
